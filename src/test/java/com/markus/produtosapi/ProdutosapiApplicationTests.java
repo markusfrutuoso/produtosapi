f0e9d5eb6fca37bcc5972282a17b8e45db9ddb7e
Markus Frutuoso <markusviniciosfr@gmail.com>
2026-08-29 00:00:13 -0300

    feat: add api

---
+++ src/test/java/com/markus/produtosapi/ProdutosapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.markus.produtosapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProdutosapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
